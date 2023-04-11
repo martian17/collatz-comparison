3c94fca88fb26375237aa94ede54d43330cb6d70
martian17 <yakumodanshi@gmail.com>
2023-04-11 22:10:55 +0900

    Implement cache

---
+++ collatz-cached.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef unsigned int uint;
+
+uint collatz(uint n0, uint* cache){
+    uint n = n0;
+    uint cnt = 0;
+    while(n != 1){
+        if(n < n0)return cnt+cache[n];
+        cnt++;
+        if(n&1){
+            n = 3*n+1;
+        }else{
+            n >>= 1;
+        }
+    }
+    return cnt;
+}
+
+int main(){
+    uint limit = 10000000;
+    //uint limit = 10;
+    uint* result = malloc(limit*sizeof(uint));
+    for(uint i = 2; i < limit; i++){
+        result[i] = collatz(i,result);
+        //printf("%u\n",result[i]);
+    }
+    printf("%u\n",result[limit-1]);
+}
+
+
